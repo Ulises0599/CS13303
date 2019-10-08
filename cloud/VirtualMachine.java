5c42139679780b8cfdb6c4e63814ea66530a0525
Juan Carlos <jc.espinosa.ceniceros@gmail.com>
2019-10-07 21:03:10 -0500

    Ajuste codigo virtual machine

--- cloud/VirtualMachine.java
+++ cloud/VirtualMachine.java
@@ -20,12 +20,17 @@ public class VirtualMachine {
 	 * HashMap<Integer, NetworkCard> 
 	 */
 	HashMap<Integer, NetworkCard> nics;  // Tarjetas de Red Virtuales
+
 	/*
 	 * VirtualMachine[10]
 	 * Agregar variable hdds de tipo 
 	 * HashMap<Integer, HardDiskDrive>
 	 */
 	HashMap<Integer, HardDiskDrive> hdds;            // Discos duros virtuales
+
+	private static final int MIN_CPUS = 1;
+	private static final long MIN_MEMORY = 1024;
+	private static final int MIN_NAME_LEN = 5; 
 
 	/*
 	 * VirtualMachine[6]
@@ -37,7 +42,23 @@ public class VirtualMachine {
 	 */
 	VirtualMachine(int cpuCount, long memoryGB, String guestOS){
 		// Escribe tu código {
-		
+		if (cpuCount < MIN_CPUS) {
+			cpuCount = MIN_CPUS;
+			System.err.println("La cantidad minima de CPUs es " + MIN_CPUS);
+		}
+		this.cpuCount = cpuCount;
+
+		if (memoryGB < MIN_MEMORY && memoryGB % MIN_MEMORY != 0) {
+			memoryGB = MIN_MEMORY;
+			System.err.println("La cantidad minima de memoria es " + MIN_MEMORY);
+			System.err.println("La cantidad de memoria debe ser multiplo de  " + MIN_MEMORY);
+		}
+		this.memoryGB = memoryGB;
+
+		if (guestOS.length() < MIN_NAME_LEN) {
+			System.err.println("La longitud minima del Guest OS es " + MIN_NAME_LEN);
+		}
+
 		this.guestOS = guestOS;
 		// }
 		this.id = CloudUtil.getUUID();
@@ -45,8 +66,29 @@ public class VirtualMachine {
 	
 	VirtualMachine(int cpuCount, long memoryGB, String name, String guestOS){
 		// Escribe tu código {
-		
+		if (cpuCount < MIN_CPUS) {
+			cpuCount = MIN_CPUS;
+			System.err.println("La cantidad minima de CPUs es " + MIN_CPUS);
+		}
+		this.cpuCount = cpuCount;
+
+		if (memoryGB < MIN_MEMORY && memoryGB % MIN_MEMORY != 0) {
+			memoryGB = MIN_MEMORY;
+			System.err.println("La cantidad minima de memoria es " + MIN_MEMORY);
+			System.err.println("La cantidad de memoria debe ser multiplo de  " + MIN_MEMORY);
+		}
+		this.memoryGB = memoryGB;
+
+		if (guestOS.length() < MIN_NAME_LEN) {
+			System.err.println("La longitud minima del Guest OS es " + MIN_NAME_LEN);
+		}
+
+		if (name.length() < MIN_NAME_LEN) {
+			System.err.println("La longitud minima del nombre es " + MIN_NAME_LEN);
+		}
+
 		this.guestOS = guestOS;
+		this.name = name;
 		// }
 		this.id = CloudUtil.getUUID();
 	}
@@ -62,9 +104,10 @@ public class VirtualMachine {
 	boolean isPoweredOn() {
 		/* Validar si la VM está prendida */
 		// Escribe tu código {
-
+		return this.powerStatus;
 		// }
-		}
+	}
+
 	/*
 	 * VirtualMachine[8]
 	 * Crear método isPoweredOff regresando 
@@ -72,14 +115,15 @@ public class VirtualMachine {
 	 */
 	boolean isPoweredOff(){
 		// Escribe tu código {
-
+		return !this.powerStatus;
 		// }
 	}
 	
 	int numberOfCPUs() {
 		/* Obtener el número de CPUs de la VM */
 		return this.cpuCount;
-		}
+	}
+
 	/* 
 	 * VirtualMachine[4]
 	 * Complementar Método powerOn: validar si la
@@ -89,20 +133,22 @@ public class VirtualMachine {
 	void powerOn() {
 		/* Encender la VM */
 		// Escribe tu código {
-
+		this.powerStatus = true;
+		System.out.println("La maquina virtual esta encendida");
 		// }
-		}
+	}
 	
 	/* 
 	 * VirtualMachine[5]
-	 * Método powerOff: validar si la VM está prendida 
+	 * Método powerOff: validar si la VM está apagada 
 	 * e imprimir mensaje informando que el estado es apagado.
 	 */
 	void powerOff() {
 		// Escribe tu código {
-
+		this.powerStatus = false;
+		System.out.println("La maquina virtual esta apagada");
 		// }
-		}
+	}
 	
 	void setName(String name) {
 		/* Asignar nombre a la VM */
@@ -116,9 +162,9 @@ public class VirtualMachine {
 	void addNIC(NetworkCard nic) {
 		/* Agregar NIC a la VM */
 		// Escribe tu código {
-
+		this.nics.put(nic.unitNumber, nic);
 		// }
-		}
+	}
 	
 	/* 
 	 * VirtualMachine [3]
@@ -128,7 +174,7 @@ public class VirtualMachine {
 	void addNICs(HashMap<Integer, NetworkCard> nics){
 		/* Inicializar nics con un arrayList*/
 		// Escribe tu código {
-
+		this.nics = nics;
 		// }
 	}
 	
@@ -140,16 +186,22 @@ public class VirtualMachine {
 	void addHDD(HardDiskDrive hdd){
 		/* Agregar HDD a la VM */
 		// Escribe tu código {
-
+		this.hdds.put(hdd.unitNumber, hdd);
 		// }	
 	}
 	
 	void addHDDs(HashMap<Integer, HardDiskDrive> hdds){
 		/* Agregar HDDs a la VM */
 		// Escribe tu código {
-
+		this.hdds = hdds;
 		// }	
 	}
+
+	String status()
+	{
+		return (isPoweredOn()) ? "Encendido" : "Apagado";
+	}
+
 	/*
 	 * VirtualMachine[7]
 	 * Sobrecargar el método java.lang.Object.toString() para mostrar
@@ -163,7 +215,14 @@ public class VirtualMachine {
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
 		// Escribe tu código {
-
+		sb.append("Nombre   : ").append(this.name).append("\n");
+		sb.append("CPU      : ").append(this.cpuCount).append("\n");
+		sb.append("Memoria  : ").append(this.memoryGB).append("\n");
+		sb.append("OS       : ").append(this.guestOS).append("\n");
+		sb.append("Estado   : ").append(status()).append("\n");
+		sb.append("ID       : ").append(this.id).append("\n");
+		sb.append("NICs     : ").append("").append("\n");
+		sb.append("HDDs     : ").append("").append("\n");
 		// }
 		return sb.toString();
 	}
