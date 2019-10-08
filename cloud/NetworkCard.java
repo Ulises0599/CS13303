8e78d7bb87681869d4b14a4f085ee73e8f660a93
Juan Carlos <jc.espinosa.ceniceros@gmail.com>
2019-10-07 20:35:33 -0500

    Ajuste codigo NetworkCard

--- cloud/NetworkCard.java
+++ cloud/NetworkCard.java
@@ -19,7 +19,8 @@ public class NetworkCard {
 	NetworkCard(int unitNumber){
 		this.unitNumber = unitNumber;
 		// Escribe tu código {
-
+		StringBuilder sb = new StringBuilder();
+		this.label = sb.append("Network Card ").append(unitNumber).toString();
 		// }
 		this.macAddress = CloudUtil.getMACAddress();
 	}
@@ -28,32 +29,39 @@ public class NetworkCard {
 		this.network = network;
 		this.unitNumber = unitNumber;
 		// Escribe tu código {
-
+		StringBuilder sb = new StringBuilder();
+		this.label = sb.append("Network Card ").append(unitNumber).toString();
 		// }
 		this.macAddress = CloudUtil.getMACAddress();
 	}
+
 	NetworkCard(String network, int unitNumber, int speedMb){
 		this.network = network;
 		this.unitNumber = unitNumber;
 		this.speedMb = speedMb;
 		// Escribe tu código {
-
+		StringBuilder sb = new StringBuilder();
+		this.label = sb.append("Network Card ").append(unitNumber).toString();
 		// }
 		this.macAddress = CloudUtil.getMACAddress();
 	}
 	
 	/*
 	 * NetworkCard[2]
-	 * obrecargar el método java.lang.Object.toString() para
+	 * Sobrecargar el método java.lang.Object.toString() para
 	 * mostrar la representación en String de NetworkCard
 	 * como lo hacía CloudUtil.printNICInfo()
 	 * (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
-	public String toString(){
+	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		// Escribe tu código {
-
+		sb.append("Etiqueta : ").append(this.label).append("\n");
+		sb.append("Unidad   : ").append(this.unitNumber).append("\n");
+		sb.append("MAC      : ").append(this.macAddress).append("\n");
+		sb.append("Network  : ").append(this.network).append("\n");
+		sb.append("Velocidad: ").append(this.speedMb).append("\n");
 		// }
 		return sb.toString();
 	}
